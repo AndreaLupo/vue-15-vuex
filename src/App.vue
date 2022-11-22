5a0e10106deac8140682536acce8a1d19a787bc8
Wolf Developer <lupo.andrea.93@gmail.com>
2022-11-22 08:51:54 +0100

    Store creation

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
-  <base-container title="Vuex"></base-container>
+  <base-container title="Vuex">
+    <the-counter></the-counter>
+    <button @click="add">Add 1</button>
+  </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
 
 export default {
   components: {
     BaseContainer,
+    TheCounter
   },
+  computed: {
+    counter() {
+      return this.$store.state.counter;
+    }
+  },
+  methods: {
+    add() {
+      this.$store.state.counter++;
+    }
+  }
 };
 </script>
 
